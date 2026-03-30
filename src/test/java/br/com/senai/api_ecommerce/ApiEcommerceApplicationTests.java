b5f3d7144f894fa4e900f870e3c9a3e7f32a5b43
Giovanna Santana <gigisantanasilva@gmail.com>
2026-03-30 08:39:07 -0300

    Conectado com o Banco de Dados

---
+++ src/test/java/br/com/senai/api_ecommerce/ApiEcommerceApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.senai.api_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEcommerceApplicationTests {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEcommerceApplication.class, args);
+	}
+
+}
