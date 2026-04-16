7c2e46bf35102f70dc4720d701a874b62fd04651
Giov4nnaxx <gigisantanasilva.gmail.com>
2026-04-16 10:48:10 -0300

    Conectando Clientes

---
+++ src/main/java/br/com/senai/api_ecommerce/cliente/DadosAtualizarCliente.java
@@ -0,0 +1,22 @@
+package br.com.senai.api_ecommerce.cliente;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record DadosAtualizarCliente(
+        Long id,
+
+        @NotBlank
+        @Size(min=3, max=100)
+        String nome,
+
+        @NotBlank
+        @Email
+        String email,
+
+        @Size(max=20)
+        String telefone
+
+) {
+}
